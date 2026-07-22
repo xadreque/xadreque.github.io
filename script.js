33487ef6bd9f12ab582ff6f0b60c575dad60abd3
xadreque <tecnicozanda@gmail.com>
2026-07-22 21:02:26 +0200

    Add DOMContentLoaded event listener for logging

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 
+// Aguarda o carregamento do DOM para garantir estabilidade de execução
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Portfólio de Xadreque Alfeu carregado com sucesso.");
+});
