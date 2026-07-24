79052fb5602580117eb9a1dd531f5253480542b0
xadreque <tecnicozanda@gmail.com>
2026-07-24 20:32:50 +0200

    Portfólio inicial

--- script.js
+++
@@ -1,5 +0,0 @@
-
-// Aguarda o carregamento do DOM para garantir estabilidade de execução
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("Portfólio de Xadreque Alfeu carregado com sucesso.");
-});
